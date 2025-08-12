355ca70b0193c6d6821922c702d0801ea084fc59
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-12 09:56:05 -0500

    getting started

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { useState } from "react"
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
+	const [count, setCount] = useState(0) // [stateVariable, setterFunction]
+
+	const eventHandler = () => {
+		setCount(count + 1)
+	}
+
+	const handleOther = () => {
+		setCount(count - 1)
+	}
+	return (
+		<>
+			<h1>Hello!</h1>
+			<p>This is amazing</p>
+			<button className="btn-secondary" onClick={eventHandler}>
+				Click me!
+			</button>
+			<p>Count: {count}</p>
+			<button className="btn-primary" onClick={handleOther}>
+				No, Click ME!!!!!
+			</button>
+		</>
+	)
 }
