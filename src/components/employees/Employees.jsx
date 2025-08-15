6b3d37331b850997f39dfdc7d595a631d22e0dde
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-15 09:14:55 -0500

    done

--- src/components/employees/Employees.jsx
+++ src/components/employees/Employees.jsx
@@ -1,11 +1,11 @@
 export const Employees = ({ state }) => {
     return (
-        <div>
-            <div>
+        <div className="flex h-50 w-100 flex-col justify-center gap-5 rounded-xl bg-white p-10">
+            <div className="flex flex-col gap-2">
                 <div className="text-xl">Name</div>
                 <div className="text-xl">{state.user.fullName}</div>
             </div>
-            <div>
+            <div className="flex flex-col gap-2">
                 <div className="text-xl">Email</div>
                 <div className="text-xl">{state.user.email}</div>
             </div>
