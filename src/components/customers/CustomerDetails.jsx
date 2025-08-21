7d66e7358656c534894243a622e6d0bd04b65941
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-21 17:37:44 -0500

    added css

--- src/components/customers/CustomerDetails.jsx
+++ src/components/customers/CustomerDetails.jsx
@@ -21,13 +21,17 @@ export const CustomerDetails = () => {
                 key={customer.id}
                 className="grid h-[90vh] place-content-center"
             >
-                <div className="flex h-80 w-200 items-center justify-evenly rounded-2xl border-2 border-black bg-blue-50">
-                    <div className="flex h-[100%] flex-col justify-evenly text-xl font-semibold">
+                <div className="flex h-100 w-200 flex-wrap items-center justify-evenly rounded-2xl border-1 border-black bg-blue-50 shadow-lg">
+                    <div className="flex h-[100%] flex-col items-start justify-center text-xl font-semibold">
+                        <p className="text-emerald-800">Name:</p>
                         <p>{customer.user.fullName}</p>
+                        <p className="mt-8 text-emerald-800">Email:</p>
                         <p>{customer.user.email}</p>
                     </div>
-                    <div className="flex h-[100%] flex-col justify-evenly text-xl font-semibold">
+                    <div className="flex h-[100%] flex-col items-start justify-center text-xl font-semibold">
+                        <p className="text-emerald-800">Address:</p>
                         <p>{customer.address}</p>
+                        <p className="mt-8 text-emerald-800">Phone Number:</p>
                         <p>{customer.phoneNumber}</p>
                     </div>
                 </div>
--- src/components/employees/EmployeeDetails.jsx
+++ src/components/employees/EmployeeDetails.jsx
@@ -17,19 +17,29 @@ export const EmployeeDetails = () => {
         })
         .map(filteredEmp => {
             return (
-                <div key={filteredEmp.id}>
-                    <div>
-                        <div>
+                <div
+                    key={filteredEmp.id}
+                    className="grid h-[90vh] place-content-center"
+                >
+                    <div className="flex h-100 w-200 flex-wrap items-center justify-evenly rounded-2xl border-1 border-black bg-blue-50 shadow-lg">
+                        <div className="flex h-20 flex-col items-start justify-evenly gap-2 p-2 text-xl font-semibold">
+                            <p className="text-emerald-800">Name:</p>
                             <p>{filteredEmp.user.fullName}</p>
+                            <p className="mt-8 text-emerald-800">Specialty:</p>
                             <p>{filteredEmp.specialty}</p>
                         </div>
-                        <div>
+                        <div className="flex h-20 flex-col items-start justify-evenly gap-2 p-2 text-xl font-semibold">
+                            <p className="text-emerald-800">Email:</p>
                             <p>{filteredEmp.user.email}</p>
+                            <p className="mt-8 text-emerald-800">Rate:</p>
                             <p>{filteredEmp.rate}</p>
                         </div>
-                        <p>
+                        <p className="w-[100%] pt-10 text-center text-xl font-semibold">
                             Working on {filteredEmp.employeeTickets.length}{" "}
-                            tickets
+                            ticket
+                            {filteredEmp.employeeTickets.length === 1
+                                ? " "
+                                : "s"}
                         </p>
                     </div>
                 </div>
