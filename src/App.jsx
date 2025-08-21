90ffec9b5f29a82e9090a824f52429837909e6c5
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-21 13:41:35 -0500

    added routes css and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { CustomersList } from "./components/customers/CustomersList.jsx"
 import { EmployeeList } from "./components/employees/EmployeeList.jsx"
+import { NavBar } from "./components/nav/NavBar.jsx"
 import { TicketList } from "./components/tickets/TicketList.jsx"
 import "./index.css"
+import { Outlet, Route, Routes } from "react-router-dom"
 
 export const App = () => {
     return (
-        <>
-            {/* <TicketList /> */}
-            {/* <CustomersList /> */}
-            <EmployeeList />
-        </>
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <>
+                        <NavBar />
+                        <Outlet />
+                    </>
+                }
+            >
+                <Route path="tickets" element={<TicketList />} />
+                <Route path="employees" element={<EmployeeList />} />
+                <Route path="customers" element={<CustomersList />} />
+            </Route>
+        </Routes>
     )
 }
